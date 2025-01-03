870ea9a39cdb5eeb960889c089d44746c8f94939
Code2With-Pratik <work.pratik1204@gmail.com>
2025-01-03 12:46:26 +0530

    uncomment Home component code in Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,24 @@
-// import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-// const Home = () => {
-//   const [roomId, setRoomId] = useState("");
-//   const navigate = useNavigate();
+const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const navigate = useNavigate();
 
-//   const handleClick = () => {
-//     navigate(`/room/${roomId}`);
-//   };
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         placeholder="Enter room id"
-//         value={roomId}
-//         onChange={(e) => setRoomId(e.target.value)}
-//       />
-//       <button onClick={handleClick}>Join Room</button>
-//     </div>
-//   );
-// };
+  const handleClick = () => {
+    navigate(`/room/${roomId}`);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter room id"
+        value={roomId}
+        onChange={(e) => setRoomId(e.target.value)}
+      />
+      <button onClick={handleClick}>Join Room</button>
+    </div>
+  );
+};
 
-// export default Home;
+export default Home;
